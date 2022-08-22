5f728c2ad895b34a5b182ed7dc5c9d332bdcb1f9
NNJio <phatsakorn.newz@gmail.com>
2022-08-22 23:46:43 +0700

    Add content

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutterlab_search_api/models/user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,24 +16,108 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    Key? key,
-  }) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<User> _users = [
+    User(
+        'Elliana Palacios',
+        '@elliana',
+        'https://images.unsplash.com/photo-1504735217152-b768bcab5ebc?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=0ec8291c3fd2f774a365c8651210a18b',
+        false),
+    User(
+        'Kayley Dwyer',
+        '@kayley',
+        'https://images.unsplash.com/photo-1503467913725-8484b65b0715?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=cf7f82093012c4789841f570933f88e3',
+        false),
+    User(
+        'Kathleen Mcdonough',
+        '@kathleen',
+        'https://images.unsplash.com/photo-1507081323647-4d250478b919?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b717a6d0469694bbe6400e6bfe45a1da',
+        false),
+    User(
+        'Kathleen Dyer',
+        '@kathleen',
+        'https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=ddcb7ec744fc63472f2d9e19362aa387',
+        false),
+    User(
+        'Mikayla Marquez',
+        '@mikayla',
+        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false),
+    User(
+        'Kiersten Lange',
+        '@kiersten',
+        'https://images.unsplash.com/photo-1542534759-05f6c34a9e63?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false),
+    User(
+        'Carys Metz',
+        '@metz',
+        'https://images.unsplash.com/photo-1516239482977-b550ba7253f2?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false),
+    User(
+        'Ignacio Schmidt',
+        '@schmidt',
+        'https://images.unsplash.com/photo-1542973748-658653fb3d12?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false),
+    User(
+        'Clyde Lucas',
+        '@clyde',
+        'https://images.unsplash.com/photo-1569443693539-175ea9f007e8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false),
+    User(
+        'Mikayla Marquez',
+        '@mikayla',
+        'https://images.unsplash.com/photo-1541710430735-5fca14c95b00?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ',
+        false)
+  ];
+
+  List<User> _foundedUser = [];
+
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _foundedUser = _users;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.grey.shade900,
+        title: Container(
+          height: 38,
+          child: TextField(
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.grey[850],
+              contentPadding: EdgeInsets.all(0),
+              prefixIcon: Icon(Icons.search,color: Colors.grey.shade500,),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50),
+                borderSide: BorderSide.none
+              ),
+              hintStyle: TextStyle(
+                fontSize: 14,
+                color: Colors.grey.shade500
+              ),
+              hintText: "ค้นหาผู้ใช้",
+            ),),
+        ),
+      ),
       body: Container(
         color: Colors.grey.shade900,
       ),
